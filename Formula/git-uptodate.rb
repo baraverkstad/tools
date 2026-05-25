d4eafa956dd2e4486d5c2d330f11fe652904137f
Per Cederberg <cederberg@gmail.com>
2026-05-25 15:24:37 +0200

    git-uptodate: update Homebrew formula for v1.1

--- Formula/git-uptodate.rb
+++ Formula/git-uptodate.rb
@@ -1,8 +1,8 @@
 class GitUptodate < Formula
   desc "Print branch, stash, and working tree status for one or more repositories"
   homepage "https://github.com/baraverkstad/tools"
-  url "https://github.com/baraverkstad/tools/archive/refs/tags/v1.0.tar.gz"
-  sha256 "0954a8041356e0879228f7ef682fb6fd3fd19eb04871d10937a115a6afeaab52"
+  url "https://github.com/baraverkstad/tools/archive/refs/tags/v1.1.tar.gz"
+  sha256 "53143c0b1f0bbcfe167ff67922bc9706e55ac1d422b27d4db4b258e10f4703b2"
   license "MIT"
 
   def install
